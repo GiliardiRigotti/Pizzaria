550fff31f61d15dcbc62f466417fc47c8c6aaf0d
Giliardi Rigotti de Oliveira <giliardi.oliveira@gmail.com>
2023-07-19 18:20:22 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql741.umbler.com", "pizzariaaraldi", "araldi2022", "pizzaria");
+    $con = mysqli_connect("mysql247.umbler.com", "pizzariaaraldi", "araldi2022", "pizzaria");
     if(!$con){
         echo "<script language='javascript' type='text/javascript'>
         alert('Erro:".mysqli_error($con)."');";
